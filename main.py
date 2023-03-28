e82653a37ba7029bd8e50b5b54ef2618c8720875
Onion-JPG <caden8898@gmail.com>
2023-03-28 11:16:50 -0400

    Documentation

--- main.py
+++ main.py
@@ -1,84 +1,108 @@
+# import libraries, customtkinter for formating, cassiopeia for API connection
 import customtkinter
-import riotAPI
 import cassiopeia
+
+# our files, riotAPI is how we get the info from the game, config contains JUST
+# an api key for the riot games API
+import riotAPI
 import config
 
+# main driver for CastHelper, we use customtkinter for ease of use and default
+# formating that looks good. I don't super understand how it works but it does!
+
+# setting default appearance and color theme to the system default
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
+# making a CTk object, setting it's defualt size to be 800 x 800 and 
+# setting its application name to Cast Helper
 root = customtkinter.CTk()
-root.geometry("700x700")
+root.geometry("800x800")
 root.title("Cast Helper")
 
+# search view class, where the search is made
 class searchView(customtkinter.CTkFrame):
+    # the init method
     def __init__(self, master=None):
         customtkinter.CTkFrame.__init__(self, master, width=600, height=400)
         self.pack(pady=20, padx=60, fill="both", expand=True)
         self.create_widgets()
 
+    # the create widgets method, we define the default screen here
     def create_widgets(self):
+        # label for search
         label = customtkinter.CTkLabel(master=self, text="Summoner Search", font=("Roboto", 24))
         label.pack(pady=12, padx=10)
 
+        # text box to input the summoner name into
         self.summonerId = customtkinter.CTkEntry(master=self, placeholder_text="Summoner Name")
         self.summonerId.pack(pady=12, padx=10)
 
+        # search button that searches
         self.searchButton = customtkinter.CTkButton(master=self, text="Search for Game", command=self.search)
         self.searchButton.pack()
 
+        # "console" text box to show result if the search doesnt work
         self.console = customtkinter.CTkTextbox(master=self, state="disabled", height=5)
         self.console.pack(pady=12, padx=10)
 
+    # the search method, we search for a summoner here
     def search(self):
-
+        # initializing variables
+        global summoner
         name = self.summonerId.get()
+
+        # config.apikey is a file that contains JUST the apikey, not included
+        # in the git for security reasons. 
         apiKey = config.apikey
 
+        # setting key and getting summoner through api
         cassiopeia.set_riot_api_key(apiKey)
-        global summoner
         summoner = cassiopeia.get_summoner(name=name, region="NA")
 
+        # if the summoner does not exist, ie. typo in name
         if (not summoner.exists): 
             self.console.configure(state="normal")
             self.console.delete("0.0", "end")
             self.console.insert("0.0", "User not found")
             self.console.configure(state="disable")
         else :
-            self.console.configure(state="normal")
-            self.console.delete("0.0", "end")
-            self.console.insert("0.0", "User found, but not in a match!")
-            self.console.configure(state="disable")
-
-            if (not summoner.current_match.exists):
+            # summoner was found but they arent in a game
+            if (not summoner.current_match.exists):            
                 self.console.configure(state="normal")
                 self.console.delete("0.0", "end")
-                self.console.insert("0.0", "user not in a match")
+                self.console.insert("0.0", "User found, but not in a match!")
                 self.console.configure(state="disable")
+            # user found and in a game, we search the summoner now
             else: 
                 self.pack_forget()
                 resultView(self.master).pack()
 
+# result view class, where the results are shown
 class resultView(customtkinter.CTkFrame):
+    # init method for result view
     def __init__(self, master=None):
         customtkinter.CTkFrame.__init__(self, master)
-
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure((0,4), weight=1)
-
         self.create_widgets()
 
+    # create widgets method for result view
     def create_widgets(self):
+        # making a new search button and placing it in the top middle
         switch_frame_button = customtkinter.CTkButton(master=self, text="Search for Another", command=self.switch_frame)
-        switch_frame_button.grid(row=0, column=2)
+        switch_frame_button.grid(row=1, column=2)
 
+        # creating a blue and red side label
         label = customtkinter.CTkLabel(master=self, text="Blue Side", font=("Roboto", 15))
         label.grid(row=1, column=0)
-
         label = customtkinter.CTkLabel(master=self, text="Red Side", font=("Roboto", 15))
         label.grid(row=1, column=4)
 
+        # calling getChamps from our riotAPI file
         riotAPI.parsing.getChamps(self, summoner)
 
+    # switch frame method forgets / deletes the info on this screen and changes over to the search view
     def switch_frame(self):
         self.pack_forget()
         searchView(self.master).pack()
--- riotAPI.py
+++ riotAPI.py
@@ -3,98 +3,73 @@ import customtkinter
 import os
 from PIL import Image
 
+# parsing class for getting info from the game
 class parsing:
+    # a method that places the champion info into the correct location in the frame
+    # could probably be done via passing current_match or each team instead of summoner
+    # but this worked and I dont see a reason to change it at the moment
+    #
+    # @param frame the fram we are puting the info into
+    # @param summoner the summoner that was searched for
     def getChamps(frame, summoner):
-        counter = 6
-        spellCount = 0
+        # row is set to 6 for formating, 6 rows from the top for the left most col
+        row = 6
+        # num is the count of the player, we use this for formating
         num = 0
-
+        # for every participant on the blue team
         for participant in summoner.current_match.blue_team.participants:
-
-            if (num < 2) :
-                frame.label = customtkinter.CTkLabel(master=frame, text=participant.summoner.name, font=("Roboto", 20))
-                frame.label.grid(row=counter, column=0)
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=cassiopeia.get_champion(key=participant.champion, region="NA").name, font=("Roboto", 18))
-                frame.label.grid(row=counter+1, column=0)
-
-                image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "champion")
-                champ_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, participant.champion.key + ".png")), size=(25, 25))
-                frame.label = customtkinter.CTkLabel(master=frame, image=champ_image, text=None)
-                frame.label.grid(row=counter+2, column=0)
-
-                spellCount = 0
-                for spell in cassiopeia.get_champion(key=participant.champion, region="NA").spells :
-                    frame.label = customtkinter.CTkLabel(master=frame, text=spell.name, font=("Roboto", 15))
-                    frame.label.grid(row=counter+3+spellCount, column=0)
-                    spellCount += 1
-            else :
-                if (num == 2) : counter = 2
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=participant.summoner.name, font=("Roboto", 20))
-                frame.label.grid(row=counter, column=1)
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=cassiopeia.get_champion(key=participant.champion, region="NA").name, font=("Roboto", 18))
-                frame.label.grid(row=counter+1, column=1)
-
-                image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "champion")
-                champ_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, participant.champion.key + ".png")), size=(25, 25))
-                frame.label = customtkinter.CTkLabel(master=frame, image=champ_image, text=None)
-                frame.label.grid(row=counter+2, column=1)
-
-                spellCount = 0
-                for spell in cassiopeia.get_champion(key=participant.champion, region="NA").spells :
-                    frame.label = customtkinter.CTkLabel(master=frame, text=spell.name, font=("Roboto", 15))
-                    frame.label.grid(row=counter+3+spellCount, column=1)
-                    spellCount += 1
-
-            counter += 8
+            if (num < 2) : 
+                parseChamp(frame, participant, 0, row)
+            else : 
+                if (num == 2) : row = 2
+                parseChamp(frame, participant, 1, row)
+            # move down 8 spaces for formating
+            row += 8
             num += 1
 
-        counter = 2
+        # row is reset to 2 for formating
+        row = 2
+        # num is reset to 0 for formating
         num = 0
+        #for every participant on the red side
         for participant in summoner.current_match.red_team.participants:
-            if (num >= 3) :
-                if (num == 3) :
-                     counter = 6
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=participant.summoner.name, font=("Roboto", 20))
-                frame.label.grid(row=counter, column=4)
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=cassiopeia.get_champion(key=participant.champion, region="NA").name, font=("Roboto", 18))
-                frame.label.grid(row=counter+1, column=4)
-
-                image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "champion")
-                champ_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, participant.champion.key + ".png")), size=(25, 25))
-                frame.label = customtkinter.CTkLabel(master=frame, image=champ_image, text=None)
-                frame.label.grid(row=counter+2, column=4)
-
-                spellCount = 0
-                for spell in cassiopeia.get_champion(key=participant.champion, region="NA").spells :
-                    frame.label = customtkinter.CTkLabel(master=frame, text=spell.name, font=("Roboto", 15))
-                    frame.label.grid(row=counter+3+spellCount, column=4)
-                    spellCount += 1
+            if (num < 3) :
+                parseChamp(frame, participant, 3, row)
             else :
-                frame.label = customtkinter.CTkLabel(master=frame, text=participant.summoner.name, font=("Roboto", 20))
-                frame.label.grid(row=counter, column=3)
-
-                frame.label = customtkinter.CTkLabel(master=frame, text=cassiopeia.get_champion(key=participant.champion, region="NA").name, font=("Roboto", 18))
-                frame.label.grid(row=counter+1, column=3)
-
-                image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "champion")
-                champ_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, participant.champion.key + ".png")), size=(25, 25))
-                frame.label = customtkinter.CTkLabel(master=frame, image=champ_image, text=None)
-                frame.label.grid(row=counter+2, column=3)
-
-                spellCount = 0
-                for spell in cassiopeia.get_champion(key=participant.champion, region="NA").spells :
-                    frame.label = customtkinter.CTkLabel(master=frame, text=spell.name, font=("Roboto", 15))
-                    frame.label.grid(row=counter+3+spellCount, column=3)
-                    spellCount += 1
-
-            counter += 8
+                if (num == 3) : row = 6
+                parseChamp(frame, participant, 4, row)
+            # move down 8 spaces for formating
+            row += 8
             num += 1
-      
 
+# a method that takes the information of the player and actually places the info into the frame
+#
+# @param frame the frame getting the info
+# @param participant the actual player that we want to get the information about
+# @param col the column that the info is getting placed into
+# @param row the first row that the info is getting placed into
+def parseChamp(frame, participant, col, row):
+    # defining the path to the folder that has all the champion icons, this can
+    # be changed to just be a API call but I found this to be easier
+    image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "champion")
 
-        
+    # using customtkinter to add the participants name
+    frame.label = customtkinter.CTkLabel(master=frame, text=participant.summoner.name, font=("Roboto", 20))
+    frame.label.grid(row=row, column=col)
+
+    # using customtkinter to add the participants champion
+    frame.label = customtkinter.CTkLabel(master=frame, text=cassiopeia.get_champion(key=participant.champion, region="NA").name, font=("Roboto", 18))
+    frame.label.grid(row=row+1, column=col)
+
+    # using customtkinter to add the champion icon
+    champ_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, participant.champion.key + ".png")), size=(25, 25))
+    frame.label = customtkinter.CTkLabel(master=frame, image=champ_image, text=None)
+    frame.label.grid(row=row+2, column=col)
+
+    # using customtkinter to add all of the champion's spells
+    spellCount = 0
+    for spell in cassiopeia.get_champion(key=participant.champion, region="NA").spells :
+        frame.label = customtkinter.CTkLabel(master=frame, text=spell.name, font=("Roboto", 15))
+        frame.label.grid(row=row+3+spellCount, column=col)
+        spellCount += 1
+    
